903bc1d6bed00f3771e18f5a8d25c8d39ced5999
Anas Mazouni <madadj4@gmail.com>
2023-03-24 23:58:40 +0100

    fix: ensure "@sentry/tracing" is imported

--- packages/bot/src/utils/sentry.ts
+++ packages/bot/src/utils/sentry.ts
@@ -1,6 +1,7 @@
 import type { Env } from '@/lib/env';
 import { version } from '@/version';
 import type * as Sentry from '@sentry/node';
+import '@sentry/tracing';
 import { ProfilingIntegration } from '@sentry/profiling-node';
 import { omit } from 'lodash';
 
@@ -9,7 +10,7 @@ export const generateSentryConfig = (env: Env, options?: Sentry.NodeOptions): Se
     dsn: env.isDev ? '' : env.SENTRY_DSN,
     environment: env.NODE_ENV,
     release: version,
-    tracesSampleRate: env.isProd ? 1 : 0,
+    tracesSampleRate: env.isProd ? 1.0 : 0,
     profilesSampleRate: env.isProd ? 1.0 : 0,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Sentry types don't export Integration[]
     integrations: [new ProfilingIntegration(), ...(options?.integrations ?? ([] as any))],
